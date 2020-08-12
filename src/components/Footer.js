ff95b2e130a85538f83ef80931803cef6dbc39a7
Mitch Henderson <shiftymitch@gmail.com>
2020-08-12 16:12:50 -0600

    Fixed dependencies, added img for Covidashboard, updated footer repo URL

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
                 <h5 className="text-light">[site & background designed by <a href="https://github.com/shiftymitch" target="_blank" rel="noopener noreferrer">Mitch Henderson</a>]</h5>
             </div>
             <p className="text-light">&copy; Copyright 2020</p>
-            <button className="btn btn-sm text-light">View This Portfolio Repo</button>
+            <a href="https://github.com/shiftymitch/react-portfolio" target="_blank" rel="noopener noreferrer" > <button className="btn btn-sm text-light">View This Portfolio Repo</button> </a>
         </div>
     </footer>;
 }
